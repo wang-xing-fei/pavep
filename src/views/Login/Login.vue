e29c7f884f4f4f9882206485f62a9c6c061b3830
wang-xing-fei <1906566025@qq.com>
2020-03-21 15:19:35 +0800

    登录界面01

---
+++ src/views/Login/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="login">
+    <div class="top">
+      <img src="@/components/img/logo01.png" alt="">
+      <img src="@/components/img/logo.png" alt="" class="img">
+    </div>
+    <div class="nav">
+      <van-tabs v-model="active">
+        <van-tab title="Account login"><Accountlogin/></van-tab>
+        <van-tab title="Code Logon">内容</van-tab>
+      </van-tabs>
+    </div>
+    <div class="footer">
+      注册
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.img{
+  width: 124px;
+  height: 124px;
+  position: absolute;
+  left: 120.5px;
+  top: 56.5px;
+}
+.nav{
+  margin-top: 77px;
+}
+</style>
+
+<script>
+import Vue from 'vue';
+import { Tab, Tabs } from 'vant';
+import Accountlogin from '@/views/Login/Accountlogin.vue'
+Vue.use(Tab);
+Vue.use(Tabs);
+export default {
+  name: 'login',
+  data() {
+    return {
+      active: 2
+    };
+  },
+  components: {
+    Accountlogin
+  }
+}
+</script>
---
+++ src/views/Login/Mobilelogin.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>手机登入</div>
+</template>
